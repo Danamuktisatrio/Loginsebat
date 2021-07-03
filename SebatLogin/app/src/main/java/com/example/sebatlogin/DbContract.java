c0ee771ad817fae24ae283c7a4176e56231c54fb
Danamuktisatrio <66562244+Danamuktisatrio@users.noreply.github.com>
2021-07-03 23:06:47 +0700

    Login

---
+++ SebatLogin/app/src/main/java/com/example/sebatlogin/DbContract.java
@@ -0,0 +1,6 @@
+package com.example.sebatlogin;
+
+public class DbContract {
+    public static final String SERVWE_LOGIN_URL = "http://192.168.1.4/Login/checkLogin.php";
+    public static final String SERVWE_REGISTER_URL = "http://192.168.1.4/Login/createData.php";
+}
